c951665a8101bb9aa2a3590be4f17fff1b596f7e
Anton Semenov <gatalsky94@yandex.ru>
2023-01-24 15:26:21 +0300

    chore: Installed dev dependencies.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +25,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Model::shouldBeStrict(! app()->isProduction());
+
+        DB::whenQueryingForLongerThan(5000, static function (Connection $connection) {
+            Log::warning("Database queries exceeded 5 seconds on {$connection->getName()}");
+        });
     }
 }
